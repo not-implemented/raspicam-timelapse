6783d080b56e239867d64d1e1084f678886a8fa5
TheTuxKeeper <thetuxkeeper@gmail.com>
2016-01-25 14:22:44 +0100

    fixed include

--- global/global.inc.sh
+++ global/global.inc.sh
@@ -2,6 +2,6 @@
 
 FILE_LOCATION=$(dirname ${BASH_SOURCE[0]})
 
-"$FILE_LOCATION"/global.conf
+. "$FILE_LOCATION"/global.conf
 
-"$FILE_LOCATION"/global-functions.sh
+. "$FILE_LOCATION"/global-functions.sh
